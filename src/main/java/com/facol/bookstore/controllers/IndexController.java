94e3ae6b5a71e8f031748f54716bfbb8f89d53be
Ryan <ryanfelipe.rr@gmail.com>
2023-12-06 20:09:26 -0300

    Controller for initial page

---
+++ src/main/java/com/facol/bookstore/controllers/IndexController.java
@@ -0,0 +1,27 @@
+package com.facol.bookstore.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Tag(name = "index")
+public class IndexController {
+
+    @GetMapping("/")
+    @Operation(
+            summary = "Home Page",
+            description = "Controller for show initial page.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful request."),
+            @ApiResponse(responseCode = "400", description = "Invalid request due to pre-defined validations."),
+            @ApiResponse(responseCode = "404", description = "Resource not found."),
+            @ApiResponse(responseCode = "500", description = "Unidentified internal error on the server."),
+    })
+    public String intialPage(){
+        return "index";
+    }
+}
