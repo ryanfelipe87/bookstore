115ed6324ad8c31277b29fff2e038a719be8de42
Ryan <ryanfelipe.rr@gmail.com>
2023-12-06 20:07:36 -0300

    Change functions

--- src/main/java/com/facol/bookstore/services/utils/PaymentServiceUtils.java
+++ src/main/java/com/facol/bookstore/services/utils/PaymentServiceUtils.java
@@ -3,20 +3,15 @@ package com.facol.bookstore.services.utils;
 import com.facol.bookstore.dtos.BookDto;
 import com.facol.bookstore.dtos.ClientDto;
 import com.facol.bookstore.exceptions.GenericNotFoundException;
-import com.facol.bookstore.patterns.singleton.LoggerSingleton;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
 
 @Service
 public class PaymentServiceUtils {
 
-    private Logger logger = LoggerSingleton.getLogger();
     private final PaymentServiceUtils paymentServiceUtils;
 
-    public PaymentServiceUtils(PaymentServiceUtils paymentServiceUtils, Logger logger){
+    public PaymentServiceUtils(PaymentServiceUtils paymentServiceUtils){
         this.paymentServiceUtils = paymentServiceUtils;
-        this.logger = logger;
     }
 
     public boolean verifyPayment(ClientDto clientDto, BookDto bookDto){
@@ -24,7 +19,7 @@ public class PaymentServiceUtils {
         double valuePay = valuePayForClient(clientDto);
 
         if(valuePay >= priceBook){
-            logger.info("Payment accept. Purchased book by: " + clientDto.getName());
+            System.out.println("Payment accept. Purchased book by: " + clientDto.getName());
             return true;
         }else{
             throw new GenericNotFoundException("Insufficient payment.");
--- src/main/java/com/facol/bookstore/services/utils/SendServiceUtils.java
+++ src/main/java/com/facol/bookstore/services/utils/SendServiceUtils.java
@@ -10,25 +10,22 @@ import java.util.logging.Logger;
 @Service
 public class SendServiceUtils {
 
-    private Logger logger = LoggerSingleton.getLogger();
-
     private final PaymentServiceUtils paymentServiceUtils;
 
-    public SendServiceUtils(PaymentServiceUtils paymentServiceUtils, Logger logger){
+    public SendServiceUtils(PaymentServiceUtils paymentServiceUtils){
         this.paymentServiceUtils = paymentServiceUtils;
-        this.logger =logger;
     }
 
     public void sendBook(ClientDto clientDto, BookDto bookDto){
         if(paymentServiceUtils.verifyPayment(clientDto, bookDto)){
-            logger.info("Payment confirmed. Book sent to address " + clientDto.getName() + " in " + clientDto.getAddress());
+            System.out.println("Payment confirmed. Book sent to address " + clientDto.getName() + " in " + clientDto.getAddress());
             updateStatusSend(bookDto);
         }else{
-            logger.info("Payment not confirmed. The book will not be sent.");
+            System.out.println("Payment not confirmed. The book will not be sent.");
         }
     }
 
     private void updateStatusSend(BookDto bookDto){
-        logger.info("The status of book was updated with ID: " + bookDto.getId());
+        System.out.println("The status of book was updated with ID: " + bookDto.getId());
     }
 }
